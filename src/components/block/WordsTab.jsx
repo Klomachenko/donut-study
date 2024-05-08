bf002f8fbbe9f2095a7af231ff693613f83460e4
Klomachenko <leekyumin0901@naver.com>
2024-05-08 12:42:33 +0900

    feat: WordsTab 구현

---
+++ src/components/block/WordsTab.jsx
@@ -0,0 +1,21 @@
+import styles from '../../../src/css/WordsTab.module.css';
+import Word from '../atom/Word';
+
+const WordsTab = () => {
+  return (
+    <div className={styles.div}>
+      <Word />
+      <Word />
+      <Word />
+      <Word />
+      <Word />
+      <Word />
+      <Word />
+      <Word />
+      <Word />
+      <Word />
+    </div>
+  );
+};
+
+export default WordsTab;
