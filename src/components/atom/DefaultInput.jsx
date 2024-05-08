c8bc1cea910582f3c791d6a6c56ab02639c5ade6
Klomachenko <leekyumin0901@naver.com>
2024-05-08 15:27:15 +0900

    feat: onKeypress Enter 변경

--- src/components/atom/DefaultInput.jsx
+++ src/components/atom/DefaultInput.jsx
@@ -1,12 +1,19 @@
 import styles from '../../../src/css/DefaultInput.module.css';
 
-const DefaultInput = ({ placeholder, value, onChange }) => {
+const DefaultInput = ({ placeholder, value, onChange, onSubmit }) => {
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      onSubmit(e);
+    }
+  };
+
   return (
     <input
       className={styles.input}
       placeholder={placeholder}
       value={value}
       onChange={onChange}
+      onKeyPress={handleKeyPress}
     ></input>
   );
 };
